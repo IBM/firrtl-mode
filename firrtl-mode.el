c6df22aa9145daf5aace6816d9f112ae88a20542
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2017-07-21 01:27:36 -0400

    Minor improvements
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- firrtl-mode.el
+++ firrtl-mode.el
@@ -1,13 +1,13 @@
 ;; See LICENSE.IBM for license details
 
 (setq firrtl-primop '("add" "sub" "mul" "div" "mod"
-                      "eq" "neq" "geq" "gt" "leq" "lt"
-                      "dshl" "dshr" "shl" "shr"
-                      "not" "and" "or" "xor" "andr" "orr" "xorr"
-                      "neg" "cvt"
-                      "asUInt" "asSInt" "asClock"
-                      "pad" "cat" "bits" "head" "tail"
-                      "mux" "validif"))
+                       "eq" "neq" "geq" "gt" "leq" "lt"
+                       "dshl" "dshr" "shl" "shr"
+                       "not" "and" "or" "xor" "andr" "orr" "xorr"
+                       "neg" "cvt"
+                       "asUInt" "asSInt" "asClock"
+                       "pad" "cat" "bits" "head" "tail"
+                       "mux" "validif"))
 (setq firrtl-keyword '("circuit" "module"
                        "input" "output"
                        "wire" "reg" "node"
@@ -19,19 +19,18 @@
 (setq firrtl-keyword-regexp (regexp-opt firrtl-keyword 'words))
 
 (setq firrtl-font-lock-keywords
-      `(
-        ;; Circuit, module declarations
+      `(;; Circuit, module declarations
         ("\\(circuit\\|module\\)\\s-+\\([^ =:;([]+\\)\\s-+:"
          (1 font-lock-keyword-face)
          (2 font-lock-function-name-face))
         ("\\(circuit\\|module\\)"
          (1 font-lock-keyword-face))
         ;; Literals
-        ("\\(\\(U\\|S\\)Int<[0-9]+>(.+?)\\)"
-         (1 font-lock-variable-name-face))
-        ;; Sized UInt, SInt
-        ("\\(\\(U\\|S\\)Int<[0-9]+>\\)"
+        ("\\(\\(U\\|S\\)Int<[0-9]+>\\)\\(.+?\\)"
          (1 font-lock-builtin-face))
+        ;; Sized UInt, SInt
+        ;; ("\\(\\(U\\|S\\)Int<[0-9]+>\\)"
+        ;;  (1 font-lock-builtin-face))
         ;; Strings
         ("\\(\".+?\"\\)"
          (1, font-lock-string-face))
@@ -40,6 +39,11 @@
          (1 font-lock-comment-face))
         (,firrtl-keyword-regexp . font-lock-type-face)
         (,firrtl-primop-regexp . font-lock-type-face)
+        ;; Indices and numbers
+        ("\\[\\([0-9]+\\)\\]"
+         (1 font-lock-variable-name-face))
+        ("\"h?[0-9]+\""
+         (0 font-lock-variable-name-face))
         ))
 
 (define-derived-mode firrtl-mode text-mode "FIRRTL mode"

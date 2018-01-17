ad6bf8a16a2f3e1b98ade564e1dee480fe7823d5
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-01-17 12:03:08 -0500

    Add autoloads
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- firrtl-mode.el
+++ firrtl-mode.el
@@ -12,7 +12,7 @@
 ;; You may obtain a copy of the License at
 ;;
 ;;   http://www.apache.org/licenses/LICENSE-2.0
-
+;;
 ;; Unless required by applicable law or agreed to in writing, software
 ;; distributed under the License is distributed on an "AS IS" BASIS,
 ;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -102,6 +102,7 @@
          (2 font-lock-type-face))
         ))
 
+;;;###autoload
 (define-derived-mode firrtl-mode text-mode "FIRRTL mode"
   "Major mode for editing Flexible Intermediate Representation of RTL (FIRRTL)"
   (setq font-lock-defaults '(firrtl-font-lock-keywords))
@@ -114,6 +115,7 @@
 (setq firrtl-type-regexp nil)
 (setq firrtl-keyword-regexp nil)
 
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.fir$" . firrtl-mode))
 
 (provide 'firrtl-mode)
